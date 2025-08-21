4e84aad8fcd2a556b577c30297b85fb9261af1b5
Your Name <you@example.com>
2025-08-21 09:14:03 +0300

    scrollbar fixed and address remove added

--- app_config.py
+++ app_config.py
@@ -2,9 +2,9 @@ import os
 import base64
 REG = True
 creds_path = os.getenv("GOOGLE_CREDS_FILENAME", "gefest-key.json")
-YANDEX_MAPS_API_KEY = os.getenv("YANDEX_MAP_API_KEY", "cfa23d17-aa11-4937-a5b5-c2f721d08e51")
-APP_SECRET_KEY = os.getenv("APP_SECRET_KEY", "agZtwYVo4y9KfdtJ348bhyoWkKM7Npu0At8v70-CYBg")
-"""with open(creds_path, "w", encoding="utf-8") as f:
-    f.write(base64.b64decode(os.getenv("GOOGLE_CREDS_JSON")).decode("utf-8"))"""
+YANDEX_MAPS_API_KEY = os.getenv("YANDEX_MAP_API_KEY", "False")
+APP_SECRET_KEY = os.getenv("APP_SECRET_KEY", "False")
+with open(creds_path, "w", encoding="utf-8") as f:
+    f.write(base64.b64decode(os.getenv("GOOGLE_CREDS_JSON")).decode("utf-8"))
 remote = False
 DB_FILE = "new.db"
